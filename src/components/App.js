b9fc09a6a1f7475c2ee021896ec97c80ecfd0199
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-01-05 17:37:22 +0100

    Setup the project structer connecting React with redux

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BooksForm from '../container/BooksForm';
+import BooksList from '../container/BooksList';
+
+const App = () => (
+  <div>
+    <BooksForm />
+    <BooksList />
+  </div>
+);
+
+export default App;
